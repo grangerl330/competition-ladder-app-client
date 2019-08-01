0be5cd5bf300659e5ca3b336c5f3146e44508031
grangerl330 <grangerl330@gmail.com>
2019-07-31 20:34:46 -0400

    added home component and Router to index.js so that NavLinks will work

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Ladder from './Ladder'
+import { NavLink, Route } from 'react-router-dom'
 
-const Home = () => {
-  return (
-    <div className="home">
-      <h1>Virtual Ladder App</h1>
-      <Ladder />
-    </div>
-  )
+class Home extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      ladders: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('/ladders')
+    .then(response => response.json())
+    .then(data => this.setState({ladders: data}))
+  }
+
+  laddersLinks = () => {
+    const laddersLinks = this.state.ladders.map(ladder =>
+      <div key={ladder.id}>
+        <p><NavLink to={`/ladders/${ladder.id}`}>{ladder.title}</NavLink></p>
+      </div>
+    )
+
+    return laddersLinks
+  }
+
+  render(){
+    return (
+      <div className="home">
+        <h1>Virtual Ladder App</h1>
+        <h2>Your Ladders</h2>
+        {this.laddersLinks()}
+      </div>
+    )
+  }
 }
 
 export default Home

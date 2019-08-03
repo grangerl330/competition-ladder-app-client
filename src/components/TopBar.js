1d124a0cc156fe122d9fc4217f6c2a0ee4c19d7b
grangerl330 <grangerl330@gmail.com>
2019-08-02 21:35:24 -0400

    updated css for top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,10 +4,8 @@ import { NavLink, Route } from 'react-router-dom'
 const TopBar = () => {
   return (
     <div className="topbar">
-      <div className="topbar-logo">
-        <div className="topbar-logo-text">
-          Virtual Ladder App
-        </div>
+      <div className="topbar-text">
+        Virtual Ladder App
       </div>
       <p>
         <NavLink to='/ladders' className='top-bar-link'>All Ladders</NavLink>

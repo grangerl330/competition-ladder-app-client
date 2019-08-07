9179d91fa938ee8be5a7a9aca1f725d1819bcf0e
grangerl330 <grangerl330@gmail.com>
2019-08-07 11:23:48 -0400

    edit to topbar display

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -9,14 +9,14 @@ const TopBar = (props) => {
         Virtual Ladder App
       </div>
       <div className="username-display">
-        <NavLink to='/profile' className="top-bar-link">{props.currentUser.username}</NavLink>
+        <NavLink to='/profile' className="white-link">{props.currentUser.username}</NavLink>
         <div className="logout-display">
           <Logout logout={props.logout}/>
         </div>
       </div>
-      <p>
-        <NavLink to='/ladders' className='top-bar-link'>All Ladders</NavLink>
-      </p>
+      <div className="topbar-link">
+        <NavLink to='/ladders' className='white-link'>All Ladders</NavLink>
+      </div>
     </div>
   )
 }
